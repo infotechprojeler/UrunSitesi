773c94773bed8f6df8281cdfc60a3a607b774c58
InfotechProjeler <ilica-18@hotmail.com>
2025-04-06 11:11:26 +0300

    6-4-25

--- UrunSitesi.MVCWebUI/Areas/Admin/Controllers/ProductsController.cs
+++ UrunSitesi.MVCWebUI/Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using UrunSitesi.Core.Entities;
 using UrunSitesi.Data;
 using UrunSitesi.MVCWebUI.Tools;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace UrunSitesi.MVCWebUI.Areas.Admin.Controllers
 {
@@ -20,13 +22,21 @@ namespace UrunSitesi.MVCWebUI.Areas.Admin.Controllers
         // GET: ProductsController
         public ActionResult Index()
         {
-            return View(_dbContext.Products);
+            var model = _dbContext.Products // veritabanından ürün listesini çek
+                .Include(c => c.Category)  // ürünlere kategorilerini dahil et
+                .Include(c => c.Brand);   // ürünlere markasını da dahil et
+            return View(model);
         }
 
         // GET: ProductsController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var model = _dbContext.Products // ürünlerden
+                .Where(c => c.Id == id) // id si route dan gelenle eşleşeni seç
+                .Include(c => c.Category) // kategorisini dahil et
+                .Include(c => c.Brand) // markasını dahil et
+                .FirstOrDefault(); // p => p.Id == id
+            return View(model);
         }
 
         // GET: ProductsController/Create
@@ -63,30 +73,57 @@ namespace UrunSitesi.MVCWebUI.Areas.Admin.Controllers
         }
 
         // GET: ProductsController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            ViewBag.Kategoriler = new SelectList(_dbContext.Categories, "Id", "Name");
+            ViewBag.BrandId = new SelectList(_dbContext.Brands, "Id", "Name");
+            if (id == null)
+                return BadRequest("Id Gereklidir!");
+            var model = _dbContext.Products.Find(id);
+            if (model == null)
+                return NotFound("Id ile eşleşen kayıt bulunamadı!");
+            return View(model);
         }
 
         // POST: ProductsController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Product collection, IFormFile? Image, bool resmiSil)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    if (Image is not null)
+                        collection.Image = FileHelper.FileLoader(Image);
+                    if (resmiSil == true)
+                    {
+                        collection.Image = string.Empty;
+                        FileHelper.FileRemover(collection.Image);
+                    }
+                    _dbContext.Products.Update(collection);
+                    _dbContext.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
             }
-            catch
-            {
-                return View();
-            }
+            ViewBag.Kategoriler = new SelectList(_dbContext.Categories, "Id", "Name");
+            ViewBag.BrandId = new SelectList(_dbContext.Brands, "Id", "Name");
+            return View(collection);
         }
 
         // GET: ProductsController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var model = _dbContext.Products // ürünlerden
+                .Where(c => c.Id == id) // id si route dan gelenle eşleşeni seç
+                .Include(c => c.Category) // kategorisini dahil et
+                .Include(c => c.Brand) // markasını dahil et
+                .FirstOrDefault(); // p => p.Id == id
+            return View(model); // modeli ekrana yolla
         }
 
         // POST: ProductsController/Delete/5
