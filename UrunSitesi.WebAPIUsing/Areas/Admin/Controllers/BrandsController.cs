38720aae9f884c99361b4d34fc6c974e5d4804e8
InfotechProjeler <ilica-18@hotmail.com>
2025-04-20 14:05:17 +0300

    20-04-2025 web api using

---
+++ UrunSitesi.WebAPIUsing/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UrunSitesi.Core.Entities;
+
+namespace UrunSitesi.WebAPIUsing.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        string _apiAdres = "https://localhost:7279/api/Brands/";
+        HttpClient httpClient = new HttpClient(); // api ye istek atmamızı sağlayacak sınıfımız.
+
+        // GET: BrandsController
+        public async Task<ActionResult> Index()
+        {
+            var model = await httpClient.GetFromJsonAsync<List<Brand>>(_apiAdres);
+            return View(model);
+        }
+
+        // GET: BrandsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BrandsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
